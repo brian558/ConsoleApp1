19ba22776f718933b028119e1a5e9f2964b0ad5c
Brian <Brian.bota@ensorinstituut.be>
2020-03-12 16:12:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taak16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo iedereen");
+            Console.WriteLine("Dit is voor de taak van pc technieken");
+            Console.WriteLine("Dit is voor de taak van pc technieken");
+        }
+    }
+}
